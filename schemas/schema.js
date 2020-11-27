857abe0042dcfbd04e6ed8df32850e12e952e480
Stanislav Taran <stastaransa1nt@gmail.com>
2020-11-27 15:02:07 +0200

    add connection to Mongo DB and models

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,21 +1,12 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLID,GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID,GraphQLInt,GraphQLList, GraphQLSchema } = graphql;
 
-const movies = [
-    {id :"1", name : 'mr Robot', genre: 'Crime', directorId: 1},
-    {id :"2", name : 'mr Geek', genre: 'Sci-fi',  directorId: 3},
-    {id :3, name : 'Fall in Love', genre: 'Romance',  directorId: 2}
-]
-
-const directors = [
-    {id: 1, name: 'Quentin Tarantino', age: 55},
-    {id: 2, name: 'Stanislav Taran', age: 44},
-    {id: 3, name: 'John Travolta', age: 45},
-]
+const Movies = require('../models/movie.model.')
+const Directors = require('../models/director.model')
 
 const MovieType = new GraphQLObjectType({
-    name: 'Movie',
+    name: "Movie",
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
@@ -23,39 +14,57 @@ const MovieType = new GraphQLObjectType({
         director:{
             type: DirectorType,
             resolve(parent,args){
-                return directors.find(movie=> movie.id == parent.id)
+                return Directors.findById(parent.directorId)
             }
         }
     }),
 });
 
 const DirectorType = new GraphQLObjectType({
-    name: 'Director',
+    name: "Director",
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        movies:{
+            type: new GraphQLList(MovieType),
+            resolve(parent, args){
+                return Movies.find({ directorId : parent.id})
+            },
+        },
     }),
 });
 
 const Query = new GraphQLObjectType({
-    name: 'Query',
+    name: "Query",
     fields: {
         movie: {
             type: MovieType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return movies.find(movie=> movie.id == args.id)
+                return Movies.findById(args.id)
             },
         },
         director: {
             type: DirectorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return directors.find(movie=> movie.id == args.id)
+                return Directors.findById(args.id)
             },
         },
-    }
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent, args) {
+                return Movies.find({})
+            },
+        },
+        directors: {
+            type: new GraphQLList(DirectorType) ,
+            resolve(parent, args) {
+                return Directors.find({})
+            },
+        },
+    },
 });
 
 module.exports = new GraphQLSchema({
