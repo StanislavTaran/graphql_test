bc65a9a83149468979c8fa30637626e6cc51b149
Stanislav Taran <stastaransa1nt@gmail.com>
2020-11-27 13:03:27 +0200

    add relative collection and query

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,11 +1,17 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID,GraphQLInt, GraphQLSchema } = graphql;
 
 const movies = [
-    {id :"1", name : 'mr Robot', genre: 'Crime'},
-    {id :"2", name : 'mr Geek', genre: 'Sci-fi'},
-    {id :3, name : 'Fall in Love', genre: 'Romance'}
+    {id :"1", name : 'mr Robot', genre: 'Crime', directorId: 1},
+    {id :"2", name : 'mr Geek', genre: 'Sci-fi',  directorId: 3},
+    {id :3, name : 'Fall in Love', genre: 'Romance',  directorId: 2}
+]
+
+const directors = [
+    {id: 1, name: 'Quentin Tarantino', age: 55},
+    {id: 2, name: 'Stanislav Taran', age: 44},
+    {id: 3, name: 'John Travolta', age: 45},
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -14,6 +20,21 @@ const MovieType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        director:{
+            type: DirectorType,
+            resolve(parent,args){
+                return directors.find(movie=> movie.id == parent.id)
+            }
+        }
+    }),
+});
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLString },
     }),
 });
 
@@ -25,6 +46,13 @@ const Query = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return movies.find(movie=> movie.id == args.id)
+            },
+        },
+        director: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return directors.find(movie=> movie.id == args.id)
             },
         },
     }
