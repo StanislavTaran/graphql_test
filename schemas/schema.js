1b50fb9bb6f30479d82768e4fed9fcb244865261
Stanislav Taran <stastaransa1nt@gmail.com>
2020-11-27 12:51:21 +0200

    setup query for movies

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,11 +1,17 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = graphql;
+
+const movies = [
+    {id :"1", name : 'mr Robot', genre: 'Crime'},
+    {id :"2", name : 'mr Geek', genre: 'Sci-fi'},
+    {id :3, name : 'Fall in Love', genre: 'Romance'}
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
     }),
@@ -16,9 +22,9 @@ const Query = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-
+                return movies.find(movie=> movie.id == args.id)
             },
         },
     }
